5fb4f3b5f3162071d178d50d68dbe24efa6da757
NikiHollywood <74148784+NikiHollywood@users.noreply.github.com>
2020-12-03 15:42:38 +0300

    Update Rubles.java

--- src/Rubles.java
+++ src/Rubles.java
@@ -1,18 +1,19 @@
 public class Rubles {
-    public static void main(String[] args) {
-        int currentBalance = 100;
-        int transactionAmount = 1100;
-        int bonus = 0;
 
-        if (transactionAmount > 1000) {
+        public static void main(String[] args) {
+            int currentBalance = 100;
+            int transactionAmount = 1100;
+            int bonus = 0;
 
-            bonus =  (int) transactionAmount / 100;
-            currentBalance = currentBalance + bonus + transactionAmount ;
+            if (transactionAmount > 1000) {
+                bonus = transactionAmount / 100;
+                currentBalance = currentBalance + bonus + transactionAmount ;
+            } else  {
+                currentBalance = currentBalance + transactionAmount;
+            }
+
+            System.out.println(currentBalance);
 
         }
 
-        System.out.println(currentBalance);
-
-
-    }
 }
